bde874e62f00a7a2c8626d14390c6a7c7a312fe7
Henrique Antunes <hinfo@gmx.com>
2017-01-14 17:46:16 -0200

    Ajustes na classe client para enviar um arquivo txt.

--- src/client/Client.java
+++ src/client/Client.java
@@ -2,16 +2,31 @@ package client;
 
 import java.io.*;
 import java.net.*;
+import javax.swing.JOptionPane;
 public class Client {
     public static void main(String[] args) {
-        String host = "192.168.0.102";
-        String comando = "imprimir";
-        Integer port = 5544;
+        String host = "192.168.0.106";
+        //String nome = "imprimir";
+        Integer port = 5100;
         
         try {
+            String nome = JOptionPane.showInputDialog(null,"Nome do arquivo");
+            File arquivo = new File(nome);
+            FileInputStream in = new FileInputStream(arquivo);
+            
             Socket s = new Socket(host, port);
-            DataOutputStream saida = new DataOutputStream(s.getOutputStream());
-            saida.writeUTF(comando);
+            OutputStream out = s.getOutputStream();
+            OutputStreamWriter saida = new OutputStreamWriter(out);
+            BufferedWriter writer = new BufferedWriter(saida);
+            writer.write(arquivo.getName()+"\n");
+            writer.flush();
+            int tamanho = 4096;
+            byte[] buffer = new byte[tamanho];  
+            int lidos = -1;
+            while ((lidos = in.read(buffer,0,tamanho)) != -1){
+                out.write(buffer,0,lidos);
+            }
+            
             
             DataInputStream entrada = new DataInputStream(s.getInputStream());
             System.out.println("Mensagem recebida de IcoPrint: \n" +
