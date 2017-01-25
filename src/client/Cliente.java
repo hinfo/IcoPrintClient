9cffaacbbd7dfe69fd8d03d664befc6873d5227f
Henrique Antunes <hinfo@gmx.com>
2017-01-25 21:00:45 -0200

    Ajustes para receber o nome da impressora como parâmetro

--- src/client/Cliente.java
+++ src/client/Cliente.java
@@ -24,20 +24,37 @@ public class Cliente {
      */
     public static void main(String[] args) throws UnknownHostException, IOException {
         
+        if (args.length < 3){
+            System.out.println("Faltam parametros!\n"
+                    + "Comando: java -jar IcoPrintClient.jar + nome_do_arquivo + ip_servidor + "
+                    + "porta + (opcional) nome_impressora");
+            System.exit(0);
+        }
         String nomeArq = args[0];
         String host = args[1]; // 127.0.0.1
         Integer port = Integer.parseInt(args[2]); // 5100
+        String nomeImpressora = "Impressora padrão";
+        
+        
+        
         File f = new File(nomeArq);
-        System.out.println("nome Arq: " + f.getName());
-        System.out.println("nome host: " + host);
-        System.out.println("nome port: " + port);
-        
-        
         Socket cliente = new Socket(host, port);
-
         ObjectOutputStream out = new ObjectOutputStream(cliente.getOutputStream());
         out.writeUTF(f.getName());
+        if (args.length > 3) { 
+            nomeImpressora = args[3];
+            ObjectOutputStream nomeImp = new ObjectOutputStream(cliente.getOutputStream());
+            nomeImp.writeUTF(nomeImpressora);
+        }
         FileInputStream fis = new FileInputStream(f);
+        
+        
+
+
+//        System.out.println("nome Arq: " + f.getName());
+//        System.out.println("nome host: " + host);
+//        System.out.println("nome port: " + port);
+//        System.out.println("nome impressora: " + nomeImpressora);
 
         int size = fis.available();
 
